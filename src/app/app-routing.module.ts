a87baf4aef745a39637725a0c1234786d336434d
bassant hebashi <bassanthebashi@gmail.com>
2022-05-20 02:10:31 +0200

    generate overview and shared modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'overview', loadChildren: () => import('./overview/overview.module').then(m => m.OverviewModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
